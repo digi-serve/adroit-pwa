df95db7e7bdc9c21add211ffd86b5ba7557ca75b
James Duncan <james@digiserve.org>
2023-02-01 16:18:50 +0700

    * better password field * no activity images screen background * optimize help images * digiserve logo in menu

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+ssh jduncan@cars.fcfthailand.org -p2222 'mkdir /data/www/nodejs/adroit/assets/pwa/new'
+tar czv www | ssh jduncan@cars.fcfthailand.org -p2222 'cat | tar xz -C /data/www/nodejs/adroit/assets/pwa/new'
+ssh jduncan@cars.fcfthailand.org -p2222 'rm -rf /data/www/nodejs/adroit/assets/pwa/www'
+ssh jduncan@cars.fcfthailand.org -p2222 'mv /data/www/nodejs/adroit/assets/pwa/new/* /data/www/nodejs/adroit/assets/pwa/.'
 scp -P 2222 version.txt jduncan@cars.fcfthailand.org:/data/www/nodejs/adroit/assets/pwa
-scp -P 2222 -r www jduncan@cars.fcfthailand.org:/data/www/nodejs/adroit/assets/pwa/www-new
-ssh jduncan@cars.fcfthailand.org -p2222 "rm -rf /data/www/nodejs/adroit/assets/pwa/www"
-ssh jduncan@cars.fcfthailand.org -p2222 "mv /data/www/nodejs/adroit/assets/pwa/www-new /data/www/nodejs/adroit/assets/pwa/www"
+ssh jduncan@cars.fcfthailand.org -p2222 'rm -rf /data/www/nodejs/adroit/assets/pwa/new'
