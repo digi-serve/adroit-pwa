c471744a1eea7127c82bc278cd19aaaa6317b1c7
James Duncan <james@digiserve.org>
2025-01-14 09:55:08 +0700

    update version

--- src/js/app.js
+++ src/js/app.js
@@ -20,7 +20,8 @@ import App from "../app.f7";
 import * as Sentry from "@sentry/browser";
 import { BrowserTracing } from "@sentry/tracing";
 
-let version = "3.0.7";
+// This needs to match the version in `/version.txt`
+let version = "3.0.8";
 
 Sentry.init({
   dsn: "https://19734818cf6847f3ae9891d1f8faee7e@o204562.ingest.sentry.io/4504251695628288",
