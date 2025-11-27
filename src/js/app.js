03c717a1c7c8245de8e2668c1e0008d1199a12fa
James Duncan <james@digiserve.org>
2025-11-27 16:23:05 +0700

    ui updates for no photos, add location and load full year

--- src/js/app.js
+++ src/js/app.js
@@ -21,7 +21,7 @@ import * as Sentry from "@sentry/browser";
 import { BrowserTracing } from "@sentry/tracing";
 
 // This needs to match the version in `/version.txt`
-let version = "3.6.2";
+let version = "3.6.4";
 
 // Framework7v8 removed version from app so we have to set it in store now
 store.dispatch("setVersion", version);
